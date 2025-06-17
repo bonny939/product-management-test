0cbee68c7be95dc7a08aa08818198d4c70a7b235
Bonface  Musila <bonface.maingi@strathmore.edu>
2025-06-17 21:17:14 +0300

    feat: add API resource classes for consistent JSON responses

---
+++ app/Http/Resources/ProductCollection.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProductCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'data' => $this->collection,
+            'meta' => [
+                'total_count' => $this->collection->count(),
+                'total_sum' => $this->collection->sum('total_value'),
+            ],
+        ];
+    }
+}
